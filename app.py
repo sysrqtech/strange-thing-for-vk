45f988a800e51cd455f5dda9d1671046dc9aef73
Cyber Tailor <CyberTailor@gmail.com>
2018-08-14 18:02:06 +0500

    first attempt

--- app.py
+++ app.py
@@ -1,39 +1,69 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
 import os
+import vk_api
 
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:////tmp/flask_app.db')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  email = db.Column(db.String(100))
-
-  def __init__(self, name, email):
-    self.name = name
-    self.email = email
+GROUP_IDS = [int(gid.strip())
+             for gid in os.environ.get("IDS", "0").slpit(',')]
+CHECK_STRINGS = os.environ.get("CHECK_STRINGS", "").split(',')
+TOKENS = os.environ.get("TOKENS", "").split(',')
 
 
-@app.route('/', methods=['GET'])
-def index():
-  return render_template('index.html', users=User.query.all())
+class Community:
+    def __init__(self, group_id: int, check_string, token):
+        self.id: int = group_id
+        self.check_string = check_string
+        self.token = token
+
+        self.api = vk_api.VkApi(token=self.token, api_version="5.80")
+
+    def mark_important(self, message_id):
+        self.api.messages.markAsImportant(message_ids=message_id, important=1)
 
 
-@app.route('/user', methods=['POST'])
-def user():
-  u = User(request.form['name'], request.form['email'])
-  db.session.add(u)
-  db.session.commit()
-  return redirect(url_for('index'))
+def get_community(event: dict):
+    group_id = event["group_id"]
+    index = GROUP_IDS.index(group_id)
+    check_string = CHECK_STRINGS[index]
+    token = TOKENS[index]
+    return Community(group_id, check_string, token)
 
-if __name__ == '__main__':
-  db.create_all()
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port, debug=True)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    event: dict = flask.request.get_json()
+    community: Community = get_community(event)
+
+    event_type = event["type"]
+    if event_type == "confirmation":
+        return community.check_string
+    if event_type == "message_new":
+        message = event["object"]
+        if not message["from_id"] == -community.id:
+            message_id = message["id"]
+            community.mark_important(message_id)
+    return "ok"
+
+
+@app.route("/callback", methods=["GET"])
+def howto():
+    return flask.Markup("""
+    1. Зайди в Настройки группы -> Работа с API -> Callback API
+    2. Вставь текущую ссылку в поле Адрес
+    3. Нажми Подтвердить
+    4. На вкладке Типы событий отметь Входящее сообщение
+    5. ????
+    6. PROFIT!
+
+    (по всем вопросам обращайтесь <a href="https://vk.me/cybertailor">к Сайберу</a>)
+    """.strip())
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
